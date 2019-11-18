58ed070969190cfbefea7fcbb872f97dbb88c613
mm3l <mm3l.info@gmail.com>
2019-11-18 16:03:53 +0100

    Update PermMissingElem.py

--- PermMissingElem/PermMissingElem.py
+++ PermMissingElem/PermMissingElem.py
@@ -16,7 +16,6 @@ def solution(A):
     n = len(A) + 1
     total_sum = n * (n + 1) // 2
 
-    print total_sum - sum(A)
     return total_sum - sum(A)
 
 #-----------------------------------------------------------------------------#
